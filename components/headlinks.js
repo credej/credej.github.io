cd6d86df0e8fc2e6a0c127b0fbfc64c7cce5ed2f
j <credej@protonmail.com>
2025-11-11 23:09:04 -0800

    Revert "improving class naming for categories"
    This reverts commit 811ebf260daa735245ed192c0672570cb3d0247a.

--- components/headlinks.js
+++ components/headlinks.js
@@ -6,10 +6,14 @@ class HeadLinks extends HTMLElement {
       <link rel="shortcut icon" type="image/png" href="favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Boldonse&family=Cutive+Mono&family=Doto:wght@100..900&family=Lusitana:wght@400;700&family=Reddit+Mono:wght@200..900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Tiny5&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Alumni+Sans+Pinstripe:ital@0;1&family=Barriecito&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Cutive+Mono&family=Doto:wght@100..900&family=Fugaz+One&family=Lexend+Exa:wght@100..900&family=Lusitana:wght@400;700&family=Reddit+Mono:wght@200..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" />
       <script src="/components/themes.js" type="text/javascript" defer></script>
+      
+      <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&family=Big+Shoulders:opsz,wght@10..72,100..900&family=Boldonse&family=Geostar&family=Martian+Mono:wght@100..800&family=Oswald:wght@200..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Tiny5&display=swap" rel="stylesheet">
     `;
   }
   constructor() {
