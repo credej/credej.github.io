b7f7366e8008dedd58033977d374592488f57dbd
j <credej@protonmail.com>
2026-01-26 22:46:47 -0800

    adding responsive file to head links

--- components/headlinks.js
+++ components/headlinks.js
@@ -2,9 +2,10 @@ class HeadLinks extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <meta name=viewport content="width=device-width,initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <script src="/components/themes.js" type="text/javascript" defer></script>
       <link rel="shortcut icon" type="image/png" href="favicon.png" />
+      <link rel="stylesheet" type="text/css" href="/css/responsive.css">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" />
       <!-- replace with open source -->
       <link rel="preconnect" href="https://fonts.googleapis.com">
