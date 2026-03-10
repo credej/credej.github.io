9f9c17ce2dbbbb73f41eb64ee419c470cf4472df
j <credej@protonmail.com>
2026-03-10 11:29:34 -0700

    testing theme behavior on live site

--- components/darktheme.js
+++ components/darktheme.js
@@ -23,7 +23,7 @@ function detectColorScheme() {
    }
 
    // if there is no preference set, the default of light will be used. apply accordingly
-   theme === 'dark' ? enableDarkMode() : disableDarkMode();
+   theme === 'light' ? enableDarkMode() : disableDarkMode();
 }
 
 // run on page load
