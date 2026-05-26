a377838941a1395246b0793d8dd6270d2bb8f132
j <credej@protonmail.com>
2026-05-26 00:02:18 -0700

    adding cli styling

--- components/header.js
+++ components/header.js
@@ -112,7 +112,7 @@ class Header extends HTMLElement {
          </div>
       </nav>
 
-<!--       <div class="footer-construction">
+      <!-- <div class="footer-construction">
          <p>Site Under Construction!</p>
       </div> -->
 
