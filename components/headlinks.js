be934d1c3c50e7924fef547415109ab6877de305
j <credej@protonmail.com>
2026-08-20 14:56:03 -0700

    cleaning up headlinks and organizing fonts

--- components/headlinks.js
+++ components/headlinks.js
@@ -6,15 +6,9 @@ class HeadLinks extends HTMLElement {
       <script src="/components/themes.js" type="text/javascript" defer></script>
       <link rel="shortcut icon" type="image/png" href="favicon.png" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" />
-      <!-- replace with open source -->
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Alumni+Sans+Pinstripe:ital@0;1&family=Barriecito&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Cutive+Mono&family=Doto:wght@100..900&family=Fugaz+One&family=Lexend+Exa:wght@100..900&family=Lusitana:wght@400;700&family=Reddit+Mono:wght@200..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&family=Big+Shoulders:opsz,wght@10..72,100..900&family=Boldonse&family=Geostar&family=Martian+Mono:wght@100..800&family=Oswald:wght@200..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Tiny5&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Anton&family=Boldonse&family=Geostar+Fill&family=Libre+Barcode+39+Text&family=PT+Mono&family=Press+Start+2P&family=Redacted&family=Redacted+Script:wght@300;400;700&family=Russo+One&family=Tektur:wght@400..900&display=swap" rel="stylesheet">
+      
       <link href="https://api.fontshare.com/v2/css?f[]=array@400&f[]=satoshi@300,700,901&f[]=clash-display@300&f[]=telma@900&f[]=author@601&f[]=alpino@500,900&f[]=comico@400&f[]=melodrama@500&f[]=boxing@400&f[]=kola@400&f[]=rx-100@400&f[]=literata@800&f[]=kihim@400&f[]=jet-brains-mono@300&f[]=roundo@500&display=swap" rel="stylesheet">
       <!-- end replace -->
     `;
