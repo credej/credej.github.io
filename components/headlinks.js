41496a0fa5173266a9d0761a02e03409f9960333
j <credej@protonmail.com>
2026-09-18 00:18:49 -0700

    re-added google fonts, improved home page styling

--- components/headlinks.js
+++ components/headlinks.js
@@ -9,6 +9,10 @@ class HeadLinks extends HTMLElement {
 
       <!-- iconoir fonts -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css"/>
+
+      <!-- google fonts -->
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Doto:wght@100..900&family=Geostar+Fill&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Press+Start+2P&family=Redacted+Script:wght@300;400;700&family=Reddit+Mono:wght@200..900&family=Tiny5&display=swap" rel="stylesheet">
     `;
   }
   constructor() {
